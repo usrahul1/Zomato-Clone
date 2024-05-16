cb138f2b12101e42e8c538f712353f2c3224e4b2
Sai Rahul Urumu <123154452+usrahul1@users.noreply.github.com>
2024-05-16 23:23:18 +0530

    Add files via upload

---
+++ codejs.js
@@ -0,0 +1,8 @@
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("show") == -1) {
+      x.className += " show";
+    } else { 
+      x.className = x.className.replace(" show", "");
+    }
+}
