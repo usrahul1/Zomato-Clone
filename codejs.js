5c09f0742f59a0da84cbb0c4caaf729f011b4900
Sai Rahul Urumu <123154452+usrahul1@users.noreply.github.com>
2024-05-17 12:50:44 +0530

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -6,3 +6,25 @@ function myFunction(id) {
       x.className = x.className.replace(" show", "");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const radio1 = document.getElementById('radio1');
+  const radio2 = document.getElementById('radio2');
+  const content1 = document.getElementById('zappmail');
+  const content2 = document.getElementById('zappnum');
+
+  function toggleContent() {
+      if (radio1.checked) {
+          content1.classList.remove('hide');
+          content2.classList.add('hide');
+      } else if (radio2.checked) {
+          content1.classList.add('hide');
+          content2.classList.remove('hide');
+      }
+  }
+
+  radio1.addEventListener('change', toggleContent);
+  radio2.addEventListener('change', toggleContent);
+
+  toggleContent();
+});
